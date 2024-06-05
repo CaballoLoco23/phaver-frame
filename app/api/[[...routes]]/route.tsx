66a1e82a92ffd29fc33680a546eac2f3553722c7
daniel plaquin <plaquindaniel@yahoo.com.ar>
2024-06-05 12:17:51 -0300

    segundo commit

--- app/api/[[...routes]]/route.tsx
+++ app/api/[[...routes]]/route.tsx
@@ -17,54 +17,34 @@ const app = new Frog({
 // export const runtime = 'edge'
 
 app.frame('/', (c) => {
-  const { buttonValue, inputText, status } = c
-  const fruit = inputText || buttonValue
+  const { buttonValue, inputText, status, } = c
+  
   return c.res({
-    image: (
-      <div
-        style={{
-          alignItems: 'center',
-          background:
-            status === 'response'
-              ? 'linear-gradient(to right, #432889, #17101F)'
-              : 'black',
-          backgroundSize: '100% 100%',
-          display: 'flex',
-          flexDirection: 'column',
-          flexWrap: 'nowrap',
-          height: '100%',
-          justifyContent: 'center',
-          textAlign: 'center',
-          width: '100%',
-        }}
-      >
-        <div
-          style={{
-            color: 'white',
-            fontSize: 60,
-            fontStyle: 'normal',
-            letterSpacing: '-0.025em',
-            lineHeight: 1.4,
-            marginTop: 30,
-            padding: '0 120px',
-            whiteSpace: 'pre-wrap',
-          }}
-        >
-          {status === 'response'
-            ? `Nice choice.${fruit ? ` ${fruit.toUpperCase()}!!` : ''}`
-            : 'Welcome!'}
-        </div>
-      </div>
-    ),
+    action: '/second',
+    image: "/img-frame1.png",
     intents: [
-      <TextInput placeholder="Enter custom fruit..." />,
-      <Button value="apples">Apples</Button>,
-      <Button value="oranges">Oranges</Button>,
-      <Button value="bananas">Bananas</Button>,
-      status === 'response' && <Button.Reset>Reset</Button.Reset>,
+
+      <Button.Link href='https://phaver.app.link/hankmoody'>Create account</Button.Link>,  
+      <Button.Link href='https://reputation.anima.io/'>Up L2</Button.Link>,
+      <Button>Meet people</Button>,
+             
     ],
   })
 })
+
+app.frame('/second', (c) => {
+  const { buttonValue, inputText, status, } = c
+  
+  return c.res({
+    
+    image: "/img-frame2.png",
+    imageAspectRatio: '1:1',
+    intents: [
+      <Button.Reset>Reset</Button.Reset>,
+    ],
+  })
+})
+
 
 devtools(app, { serveStatic })
 
